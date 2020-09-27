9be5c6f8099c68f9fb829180f97a2f8c732d40f1
Faris Ansari <netchamp.faris@gmail.com>
2020-09-27 16:46:16 +0530

    feat: New Page
    - Create new page
    - Remove description field
    - Cleanup compare page

--- wiki/wiki/doctype/wiki_page/wiki_page.py
+++ wiki/wiki/doctype/wiki_page/wiki_page.py
@@ -9,6 +9,13 @@ from frappe.website.website_generator import WebsiteGenerator
 
 
 class WikiPage(WebsiteGenerator):
+	def after_insert(self):
+		revision = frappe.new_doc("Wiki Page Revision")
+		revision.wiki_page = self.name
+		revision.content = self.content
+		revision.message = "Create Wiki Page"
+		revision.insert()
+
 	def set_route(self):
 		if self.is_website_published() and not self.route:
 			self.route = self.make_route()
@@ -37,12 +44,17 @@ class WikiPage(WebsiteGenerator):
 		self.save()
 
 	def get_context(self, context):
-		context.docs_search_scope = frappe.db.get_single_value("Wiki Settings", "home_route")
+		context.home_route = frappe.db.get_single_value("Wiki Settings", "home_route")
+		context.docs_search_scope = context.home_route
 		context.can_edit = frappe.session.user != "Guest"
 
 		if frappe.form_dict:
-			context.parents = [{"route": "/" + self.route, "label": "Go to Page"}]
+			context.parents = [{"route": "/" + self.route, "label": self.title}]
 			context.add_breadcrumbs = True
+
+		if frappe.form_dict.new:
+			context.title = "New Wiki Page"
+			return
 
 		if frappe.form_dict.edit:
 			context.title = "Editing " + self.title
@@ -61,15 +73,19 @@ class WikiPage(WebsiteGenerator):
 		if frappe.form_dict.compare:
 			from ghdiff import diff
 
-			context.title = "Revision: " + self.title
-
 			revision = frappe.form_dict.compare
+			context.title = "Revision: " + revision
+			context.parents = [
+				{"route": "/" + self.route, "label": self.title},
+				{"route": "/" + self.route + "?revisions=true", "label": "Revisions"},
+			]
+
 			revision = frappe.get_doc("Wiki Page Revision", revision)
 
 			context.revision = revision
 			previous_revision_content = frappe.db.get_value(
 				"Wiki Page Revision",
-				filters={"creation": ("<", revision.creation)},
+				filters={"creation": ("<", revision.creation), "wiki_page": self.name},
 				fieldname=["content"],
 				order_by="creation asc",
 			)
@@ -80,7 +96,7 @@ class WikiPage(WebsiteGenerator):
 			context.diff = diff(previous_revision_content, revision.content, css=False)
 			return
 
-		context.metatags = {"title": self.title, "description": self.description}
+		context.metatags = {"title": self.title}
 		sidebar = frappe.db.get_single_value("Wiki Settings", "sidebar")
 		context.sidebar_items = frappe.get_doc("Website Sidebar", sidebar).get_items()
 		context.last_revision = self.get_last_revision()
@@ -110,3 +126,36 @@ def update(wiki_page, title, content, edit_message):
 
 	frappe.response.location = "/" + wiki_page.route
 	frappe.response.type = "redirect"
+
+
+@frappe.whitelist(methods=["POST"], alias="new-page")
+def update(title, route, content):
+	wiki_page = frappe.new_doc("Wiki Page")
+	wiki_page.title = title
+	wiki_page.route = route
+	wiki_page.content = content
+	wiki_page.published = True
+	wiki_page.insert()
+
+	frappe.response.location = "/" + wiki_page.route
+	frappe.response.type = "redirect"
+
+
+@frappe.whitelist(alias="get-route")
+def get_route(title):
+	import re
+
+	# lowercase
+	route = title.lower()
+	# remove special characters
+	route = re.sub(r"[^0-9a-zA-Z]", " ", route)
+	# spaces to hyphens
+	route = "-".join(route.split())
+	# limit to 2000 chars
+	route = route.strip("/.")[:2000]
+
+	wiki_home_route = frappe.db.get_single_value("Wiki Settings", "home_route")
+	if not route.startswith(wiki_home_route + "/"):
+		route = wiki_home_route + "/" + route
+
+	return route
