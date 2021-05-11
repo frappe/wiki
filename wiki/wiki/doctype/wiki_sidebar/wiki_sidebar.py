f984221e34890f0fde5b6f1c088e6af63fe315e3
hasnain2808 <hasnain2808@gmail.com>
2021-05-12 03:46:50 +0530

    feat: full sidebars

--- wiki/wiki/doctype/wiki_sidebar/wiki_sidebar.py
+++ wiki/wiki/doctype/wiki_sidebar/wiki_sidebar.py
@@ -8,29 +8,60 @@ from frappe.utils.nestedset import NestedSet
 
 class WikiSidebar(NestedSet):
 	def get_items(self):
-			items = frappe.get_all(
-				"Wiki Sidebar Item",
-				filters={'parent': self.name},
-				fields=["title", "route", ],
-				order_by="idx asc",
-			)
+		items = frappe.get_all(
+			"Wiki Sidebar Item",
+			filters={'parent': self.name},
+			fields=["title", "route", ],
+			order_by="idx asc",
+		)
 
-			items_by_group = {}
-			items_without_group = []
+		items_by_group = {}
+		items_without_group = []
 
-			for item in items:
-				item.route =  '/' + item.route
-				items_without_group.append(item)
+		for item in items:
+			item.route =  '/' + item.route
+			items_without_group.append(item)
 
-			child_sidebars = frappe.get_all(
+
+
+
+		child_sidebars = frappe.get_all(
+			"Wiki Sidebar",
+			filters={'parent_wiki_sidebar': self.name},
+			fields=["title" , "name"],
+			order_by="idx asc",
+		)
+
+		if not child_sidebars:
+			siblings = frappe.get_all(
 				"Wiki Sidebar",
-				filters={'parent_wiki_sidebar': self.name},
+				filters={'parent_wiki_sidebar': self.parent_wiki_sidebar},
 				fields=["title" , "name"],
 				order_by="idx asc",
 			)
 
-			print("child_sidebars")
-			print(child_sidebars)
+			for child_sidebar in siblings:
+				items = frappe.get_all(
+					"Wiki Sidebar Item",
+					filters={'parent': child_sidebar.name},
+					fields=["title", "route", "parent"],
+					order_by="idx asc",
+				)
+
+		
+
+				for item in items:
+					item.group=child_sidebar.title
+					item.route =  '/' + item.route
+					items_by_group.setdefault(child_sidebar.title, []).append(item)
+
+			out = []
+			for group, items in items_by_group.items():
+				out.append({"group_title": group, "group_items": items})
+			return out
+
+		else:
+
 			for child_sidebar in child_sidebars:
 				items = frappe.get_all(
 					"Wiki Sidebar Item",
@@ -39,20 +70,17 @@ class WikiSidebar(NestedSet):
 					order_by="idx asc",
 				)
 
-				print("items")
-				print(items)
-
 				for item in items:
 					item.group=child_sidebar.title
 					item.route =  '/' + item.route
 					items_by_group.setdefault(child_sidebar.title, []).append(item)
 
-			out = []
+		out = []
+		# out += items_without_group
+		out.append({"group_title": self.title, "group_items": items_without_group})
 
-			for group, items in items_by_group.items():
-				out.append({"group_title": group, "group_items": items})
-			print("out")
-			print(out)
-			out += items_without_group
+		for group, items in items_by_group.items():
+			out.append({"group_title": group, "group_items": items})
+		
 
-			return out
+		return out
