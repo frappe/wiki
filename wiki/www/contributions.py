6ddf4260108a1aa45ab1c4426c40fc12e561bd76
hasnain2808 <hasnain2808@gmail.com>
2021-07-20 21:45:16 +0530

    feat: contributions page design

--- wiki/www/contributions.py
+++ wiki/www/contributions.py
@@ -7,16 +7,17 @@ def get_context(context):
 	context.no_sidebar = 1
 	color_map = {
 		'Changes Requested': 'blue',
-		'Under Review': 'pink',
+		'Under Review': 'orange',
 		'Rejected': 'red',
 		'Approved': 'green',
 	}
 
 	context.contributions = []
-	contributions = frappe.get_list("Wiki Page Patch", ["message", "status", "name", "wiki_page"])
+	contributions = frappe.get_list("Wiki Page Patch", ["message", "status", "name", "wiki_page", 'creation'])
 	for contribution in contributions:
 		route = frappe.db.get_value("Wiki Page", contribution.wiki_page, "route")
 		contribution.edit_link = '/edit?wiki_page='+ route + '&' + "edit=true&wiki_page_patch=" + contribution.name
 		contribution.color = color_map[contribution.status]
+		contribution.creation = frappe.utils.pretty_date(contribution.creation)
 		context.contributions.extend([contribution])
 	return context
