d628a25d721474eb52ae0202181c782526838e00
hasnain2808 <hasnain2808@gmail.com>
2021-07-27 13:16:44 +0530

    fix: new page path

--- wiki/public/js/edit_wiki.js
+++ wiki/public/js/edit_wiki.js
@@ -22,12 +22,6 @@ window.EditWiki = class EditWiki {
   add_new_link() {
     let $new_page = $(`
 			<li class="sidebar-item" data-type="Wiki Page" data-name="new" data-new=1 ><div>
-					<div style="align-items: center;">
-							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" 
-									class="bi bi-dot" viewBox="0 0 16 16">
-									<path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path>
-							</svg>
-					</div>
 					<div>
 							<a href="#" class="green">New Wiki Page</a>
 					</div></div>
