e1bad994000c93012c33bd87870c9d7fabe0ea8d
Mohammad Hasnain <hasnain2808@gmail.com>
2021-09-06 22:52:53 +0530

    feat: pagination for contributions page

--- wiki/www/contributions.py
+++ wiki/www/contributions.py
@@ -16,7 +16,40 @@ def get_context(context):
 		"Wiki Page Patch",
 		["message", "status", "name", "wiki_page", "creation", "new"],
 		order_by="modified desc",
-		filters= [['status', '!=', 'Approved']]
+		limit=10,
+		filters=[["status", "!=", "Approved"]],
+	)
+	for contribution in contributions:
+		route = frappe.db.get_value("Wiki Page", contribution.wiki_page, "route")
+		if contribution.new:
+			contribution.edit_link = f"/{route}/new?wiki_page_patch={contribution.name}"
+		else:
+			contribution.edit_link = f"/{route}/edit?wiki_page_patch={contribution.name}"
+		contribution.color = color_map[contribution.status]
+		contribution.creation = frappe.utils.pretty_date(contribution.creation)
+		context.contributions.extend([contribution])
+	return context
+
+
+@frappe.whitelist()
+def get_contributions(limit):
+	context = frappe._dict()
+	context.no_cache = 1
+	context.no_sidebar = 1
+	color_map = {
+		"Changes Requested": "blue",
+		"Under Review": "orange",
+		"Rejected": "red",
+		"Approved": "green",
+	}
+
+	context.contributions = []
+	contributions = frappe.get_list(
+		"Wiki Page Patch",
+		["message", "status", "name", "wiki_page", "creation", "new"],
+		order_by="modified desc",
+		limit=limit,
+		filters=[["status", "!=", "Approved"]],
 	)
 	for contribution in contributions:
 		route = frappe.db.get_value("Wiki Page", contribution.wiki_page, "route")
