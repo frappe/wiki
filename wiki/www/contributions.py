d891fca4149835c50cf61b953f7b59c9113c27e8
phot0n <ritwikpuri5678@gmail.com>
2022-08-14 12:45:46 +0530

    chore: keep get_context on top in contributions.py & drafts.py

--- wiki/www/contributions.py
+++ wiki/www/contributions.py
@@ -12,28 +12,6 @@ color_map = {
 }
 
 
-def get_user_contributions(limit):
-	contributions = []
-	wiki_page_patches = frappe.get_list(
-		"Wiki Page Patch",
-		["message", "status", "name", "wiki_page", "creation", "new"],
-		order_by="modified desc",
-		limit=cint(limit),
-		filters=[["status", "!=", "Draft"], ["owner", '=', frappe.session.user]],
-	)
-	for wiki_page_patch in wiki_page_patches:
-		route = frappe.db.get_value("Wiki Page", wiki_page_patch.wiki_page, "route")
-		if wiki_page_patch.new:
-			wiki_page_patch.edit_link = f"/{route}/new-wiki?wiki_page_patch={wiki_page_patch.name}"
-		else:
-			wiki_page_patch.edit_link = f"/{route}/edit-wiki?wiki_page_patch={wiki_page_patch.name}"
-		wiki_page_patch.color = color_map[wiki_page_patch.status]
-		wiki_page_patch.creation = frappe.utils.pretty_date(wiki_page_patch.creation)
-		contributions.extend([wiki_page_patch])
-
-	return contributions
-
-
 def get_context(context):
 	context.pilled_title = "My Contributions"
 	context.no_cache = 1
@@ -58,3 +36,25 @@ def get_context(context):
 @frappe.whitelist()
 def get_contributions(limit):
 	return {"contributions": get_user_contributions(limit)}
+
+
+def get_user_contributions(limit):
+	contributions = []
+	wiki_page_patches = frappe.get_list(
+		"Wiki Page Patch",
+		["message", "status", "name", "wiki_page", "creation", "new"],
+		order_by="modified desc",
+		limit=cint(limit),
+		filters=[["status", "!=", "Draft"], ["owner", '=', frappe.session.user]],
+	)
+	for wiki_page_patch in wiki_page_patches:
+		route = frappe.db.get_value("Wiki Page", wiki_page_patch.wiki_page, "route")
+		if wiki_page_patch.new:
+			wiki_page_patch.edit_link = f"/{route}/new-wiki?wiki_page_patch={wiki_page_patch.name}"
+		else:
+			wiki_page_patch.edit_link = f"/{route}/edit-wiki?wiki_page_patch={wiki_page_patch.name}"
+		wiki_page_patch.color = color_map[wiki_page_patch.status]
+		wiki_page_patch.creation = frappe.utils.pretty_date(wiki_page_patch.creation)
+		contributions.extend([wiki_page_patch])
+
+	return contributions
--- wiki/www/drafts.py
+++ wiki/www/drafts.py
@@ -5,28 +5,6 @@ from wiki.wiki.doctype.wiki_page.wiki_page import get_open_contributions
 from wiki.wiki.doctype.wiki_page.wiki_page import get_open_drafts
 
 
-def get_user_drafts(limit):
-	drafts = []
-	wiki_page_patches = frappe.get_list(
-		"Wiki Page Patch",
-		["message", "status", "name", "wiki_page", "creation", "new"],
-		order_by="modified desc",
-		limit=cint(limit),
-		filters=[["status", "=", "Draft"], ["owner", '=', frappe.session.user]],
-	)
-	for wiki_page_patch in wiki_page_patches:
-		route = frappe.db.get_value("Wiki Page", wiki_page_patch.wiki_page, "route")
-		if wiki_page_patch.new:
-			wiki_page_patch.edit_link = f"/{route}/new-wiki?wiki_page_patch={wiki_page_patch.name}"
-		else:
-			wiki_page_patch.edit_link = f"/{route}/edit-wiki?wiki_page_patch={wiki_page_patch.name}"
-		wiki_page_patch.color = "orange"
-		wiki_page_patch.creation = frappe.utils.pretty_date(wiki_page_patch.creation)
-		drafts.extend([wiki_page_patch])
-
-	return drafts
-
-
 def get_context(context):
 	context.pilled_title = "My Drafts"
 	context.no_cache = 1
@@ -51,3 +29,25 @@ def get_context(context):
 @frappe.whitelist()
 def get_drafts(limit):
 	return {"contributions": get_user_drafts(limit)}
+
+
+def get_user_drafts(limit):
+	drafts = []
+	wiki_page_patches = frappe.get_list(
+		"Wiki Page Patch",
+		["message", "status", "name", "wiki_page", "creation", "new"],
+		order_by="modified desc",
+		limit=cint(limit),
+		filters=[["status", "=", "Draft"], ["owner", '=', frappe.session.user]],
+	)
+	for wiki_page_patch in wiki_page_patches:
+		route = frappe.db.get_value("Wiki Page", wiki_page_patch.wiki_page, "route")
+		if wiki_page_patch.new:
+			wiki_page_patch.edit_link = f"/{route}/new-wiki?wiki_page_patch={wiki_page_patch.name}"
+		else:
+			wiki_page_patch.edit_link = f"/{route}/edit-wiki?wiki_page_patch={wiki_page_patch.name}"
+		wiki_page_patch.color = "orange"
+		wiki_page_patch.creation = frappe.utils.pretty_date(wiki_page_patch.creation)
+		drafts.extend([wiki_page_patch])
+
+	return drafts
