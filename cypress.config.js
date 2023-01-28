c4bec0902701763a9d1501b67e2e1d2c70d7f7fb
Bread Genie <msuhailbh07@gmail.com>
2023-01-28 23:08:56 +0530

    ci: use different cypress key and id

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: "y1k5pn",
+    projectId: "w2jgcb",
     adminPassword: "admin",
     setupNodeEvents(on, config) {
       // implement node event listeners here
