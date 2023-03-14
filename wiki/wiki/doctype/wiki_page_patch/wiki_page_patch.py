frappe/wiki

commit 6fd5ed729e9660d8bc92b996148a0c95c3abe19b

feat: set `topmost` to wiki

wiki/wiki/doctype/wiki_page_patch/wiki_page_patch.py
@@ -37,8 +37,6 @@ class WikiPagePatch(Document):
 
 		if self.new:
 			self.create_new_wiki_page()
-		else:
-			self.update_old_page()
 
 		if self.sidebar_edited:
 			self.update_sidebars()
@@ -63,15 +61,6 @@ class WikiPagePatch(Document):
 
 		self.new_wiki_page.update(wiki_page_dict)
 		self.new_wiki_page.save()
-
-	def update_old_page(self):
-		self.wiki_page_doc.update_page(self.new_title, self.new_code, self.message, self.raised_by)
-		updated_page = frappe.get_all(
-			"Wiki Sidebar Item", {"item": self.wiki_page, "type": "Wiki Page"}, pluck="name"
-		)
-		for page in updated_page:
-			frappe.db.set_value("Wiki Sidebar Item", page, "title", self.new_title)
-		return
 
 	def update_sidebars(self):
 		if not self.new_sidebar_items:

wiki/wiki/doctype/wiki_page/wiki_page.py
@@ -140,7 +140,7 @@ class WikiPage(WebsiteGenerator):
 		context.dark_mode_logo = wiki_settings.dark_mode_logo
 		context.add_dark_mode = wiki_settings.add_dark_mode
 		context.script = wiki_settings.javascript
-		context.docs_search_scope = self.get_docs_search_scope()
+		context.docs_search_scope = "wiki"
 		context.metatags = {
 			"title": self.title,
 			"description": self.meta_description,
@@ -180,19 +180,8 @@ class WikiPage(WebsiteGenerator):
 			}
 		)
 
-	def get_docs_search_scope(self):
-		sidebar = frappe.get_all(
-			doctype="Wiki Sidebar Item",
-			fields=["name", "parent"],
-			filters=[["item", "=", self.name]],
-		)
-		topmost = ""
-		if sidebar:
-			topmost = frappe.get_doc("Wiki Sidebar", sidebar[0].parent).find_topmost(sidebar[0].parent)
-		return topmost
-
 	def get_items(self, sidebar_items):
-		topmost = "/wiki"
+		topmost = "wiki"
 
 		sidebar_html = frappe.cache().hget("wiki_sidebar", topmost)
 		if not sidebar_html or frappe.conf.disable_website_cache or frappe.conf.developer_mode:

wiki/wiki/doctype/wiki_sidebar/wiki_sidebar.py
@@ -8,13 +8,11 @@ from frappe.model.document import Document
 
 class WikiSidebar(Document):
 	def get_items(self):
-		topmost = self.find_topmost(self.name)
+		topmost = "wiki"
 
 		sidebar_html = frappe.cache().hget("wiki_sidebar", topmost)
 		if not sidebar_html or frappe.conf.disable_website_cache or frappe.conf.developer_mode:
-			sidebar_items = frappe.get_doc("Wiki Sidebar", topmost).get_children()
 			context = frappe._dict({})
-			context.sidebar_items = sidebar_items
 			context.docs_search_scope = topmost
 			sidebar_html = frappe.render_template(
 				"wiki/wiki/doctype/wiki_page/templates/web_sidebar.html", context
@@ -29,12 +27,6 @@ class WikiSidebar(Document):
 	def on_update(self):
 		self.clear_cache()
 
-	def find_topmost(self, me):
-		parent = frappe.db.get_value("Wiki Sidebar Item", {"item": me, "type": "Wiki Sidebar"}, "parent")
-		if not parent:
-			return me
-		return self.find_topmost(parent)
-
 	def clear_cache(self):
-		topmost = self.find_topmost(self.name)
+		topmost = "wiki"
 		frappe.cache().hdel("wiki_sidebar", topmost)

commit 6b54f28330db4172b0bc95074a1fd52c31039aaf

fix: editing pages not working
Caused by 6fd5ed7 :D

wiki/wiki/doctype/wiki_page_patch/wiki_page_patch.py
@@ -37,6 +37,8 @@ class WikiPagePatch(Document):
 
 		if self.new:
 			self.create_new_wiki_page()
+		else:
+			self.update_old_page()
 
 		if self.sidebar_edited:
 			self.update_sidebars()
@@ -61,6 +63,9 @@ class WikiPagePatch(Document):
 
 		self.new_wiki_page.update(wiki_page_dict)
 		self.new_wiki_page.save()
+
+	def update_old_page(self):
+		self.wiki_page_doc.update_page(self.new_title, self.new_code, self.message, self.raised_by)
 
 	def update_sidebars(self):
 		if not self.new_sidebar_items: