1a989cc61cf368bcb77d2609081d9fcb78bcd2ea
Bread Genie <msuhailbh07@gmail.com>
2023-03-20 14:21:26 +0530

    style: edit mode button group revampy

--- wiki/public/js/render_wiki.js
+++ wiki/public/js/render_wiki.js
@@ -277,7 +277,7 @@ window.RenderWiki = class RenderWiki extends Wiki {
           <span>Add Page</span>
         </div>
         <div class="text-muted discard-sidebar small">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
           <span>Discard</span>
         </div>
         <div class="text-muted save-sidebar small">
