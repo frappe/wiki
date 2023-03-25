db821718e39fe23fc3ea6431412ebca5222e8d7a
Bread Genie <msuhailbh07@gmail.com>
2023-03-25 17:25:12 +0530

    test: remove custom viewport values

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,6 @@ module.exports = defineConfig({
   e2e: {
     projectId: "w2jgcb",
     adminPassword: "admin",
-    viewportWidth: 1440,
-    viewportHeight: 900,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
