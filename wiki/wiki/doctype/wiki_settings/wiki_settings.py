cb3f0f200ed0cbc4c486c70d88129c7d936b9556
Bread Genie <msuhailbh07@gmail.com>
2023-04-11 17:43:08 +0530

    fix: `Wiki Settings` `default_wiki_space` autocomplete

--- wiki/wiki/doctype/wiki_settings/wiki_settings.py
+++ wiki/wiki/doctype/wiki_settings/wiki_settings.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2020, Frappe and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class WikiSettings(Document):
 	pass
+
+
+@frappe.whitelist()
+def get_all_spaces():
+	return frappe.get_all("Wiki Space", pluck="route")
