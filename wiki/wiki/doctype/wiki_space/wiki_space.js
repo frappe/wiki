90ada5ec895b2bbf60f217717d8b03953d01e466
Bread Genie <msuhailbh07@gmail.com>
2023-04-11 17:02:44 +0530

    feat: add `See on Website` from `Wiki Space`

--- wiki/wiki/doctype/wiki_space/wiki_space.js
+++ wiki/wiki/doctype/wiki_space/wiki_space.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2023, Frappe and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Wiki Space", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Wiki Space", {
+  refresh(frm) {
+    frm.add_web_link(`/${frm.doc.route}`, __("See on website"));
+  },
+});
