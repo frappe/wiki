ee702807f058265b8eccd08abeadb31e2a29dc9a
Bread Genie <msuhailbh07@gmail.com>
2023-04-11 14:18:26 +0530

    fix: `after_install` hook

--- wiki/install.py
+++ wiki/install.py
@@ -9,16 +9,21 @@ def after_install():
 	# create the wiki homepage
 	page = frappe.new_doc("Wiki Page")
 	page.title = "Home"
-	page.route = "home"
+	page.route = "wiki/home"
 	page.content = "Welcome to the homepage of your wiki!"
 	page.published = True
 	page.insert()
+
+	# create the wiki space
+	space = frappe.new_doc("Wiki Space")
+	space.route = "wiki"
+	space.insert()
 
 	# create the wiki sidebar
 	sidebar = frappe.new_doc("Wiki Group Item")
 	sidebar.wiki_page = page.name
 	sidebar.parent_label = "Wiki"
-	sidebar.parent = "Wiki Settings"
-	sidebar.parenttype = "Wiki Settings"
-	sidebar.parentfield = "wiki_sidebar"
+	sidebar.parent = (space.name,)
+	sidebar.parenttype = "Wiki Space"
+	sidebar.parentfield = "wiki_sidebars"
 	sidebar.insert()
