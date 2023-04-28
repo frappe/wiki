7c0a855ac0b6db541104f4e046ee6d2904fd115c
Bread Genie <msuhailbh07@gmail.com>
2023-04-28 20:43:58 +0530

    chore: only accept image while trying to upload images

--- wiki/public/js/editor.js
+++ wiki/public/js/editor.js
@@ -223,6 +223,7 @@ buttons.alignRight.addEventListener("click", () => {
 buttons.image.addEventListener("click", () => {
   const input = document.createElement("input");
   input.type = "file";
+  input.accept = "image/*";
 
   input.onchange = (e) => {
     const file = e.target.files[0];
