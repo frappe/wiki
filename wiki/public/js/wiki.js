7268c1e4c769c6593c20929bfdb6b2d4b96f4c05
Bread Genie <msuhailbh07@gmail.com>
2023-05-23 19:33:32 +0530

    feat: copy to clipboard for code blocks
    Fix #131

--- wiki/public/js/wiki.js
+++ wiki/public/js/wiki.js
@@ -106,4 +106,16 @@ window.Wiki = class Wiki {
         $($heading).append($a);
       });
   }
+
+  add_click_to_copy() {
+    $("pre code")
+      .parent("pre")
+      .prepend(
+        `<button title="Copy Code" class="btn copy-btn" data-toggle="tooltip"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg></button>`,
+      );
+
+    $(".copy-btn").on("click", function () {
+      frappe.utils.copy_to_clipboard($(this).siblings("code").text());
+    });
+  }
 };
