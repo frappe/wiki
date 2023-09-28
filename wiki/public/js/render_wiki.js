c931453b80e7373c155d11d94ff335905b0ebdee
Saqib Ansari <nextchamp.saqib@gmail.com>
2023-09-28 13:10:28 +0530

    refactor: sidebar css refresh with espresso designs

--- wiki/public/js/render_wiki.js
+++ wiki/public/js/render_wiki.js
@@ -341,7 +341,7 @@ window.RenderWiki = class RenderWiki extends Wiki {
   add_trash_icon() {
     const trashIcon = `<div class="text-muted hide remove-sidebar-item small">
       <span class="trash-icon">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+        <svg viewBox="0 0 24 24" width="16" height="16" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
       </span>
     </div>`;
 
