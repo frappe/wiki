ba5dbca14d975333b07eb457bd35a59a596f599f
Bread Genie <msuhailbh07@gmail.com>
2023-12-05 12:35:51 +0530

    ci(ui-test): figure out port from `common_site_config`

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require("cypress");
+const { webserver_port } = require("../../sites/common_site_config.json");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://wiki.test:8000",
+    baseUrl: `http://wiki.test:${webserver_port}`,
     projectId: "w2jgcb",
     adminPassword: "admin",
     viewportWidth: 1100,
