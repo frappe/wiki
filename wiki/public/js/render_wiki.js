8c4326872186a2165e30fe5f69cd66aae5545860
Bread Genie <msuhailbh07@gmail.com>
2024-04-16 12:56:17 +0530

    feat(wiki-page): page settings

--- wiki/public/js/render_wiki.js
+++ wiki/public/js/render_wiki.js
@@ -109,6 +109,7 @@ window.RenderWiki = class RenderWiki extends Wiki {
         this.set_revisions();
         this.add_click_to_copy();
         this.setup_feedback();
+        this.setup_page_settings();
       }
     });
   }
@@ -716,6 +717,32 @@ window.RenderWiki = class RenderWiki extends Wiki {
           $(".ratings-number").removeClass("rating-active");
           $(".long-feedback").val("");
           $(".feedback-email").val("");
+        });
+    });
+  }
+
+  setup_page_settings() {
+    $(".update-page-settings-button").on("click", function () {
+      const name = $('[name="wiki-page-name"]').val();
+      const hideOnSidebar = $('input[name="pageHideOnSidebar"]').prop(
+        "checked",
+      );
+
+      frappe
+        .call({
+          method: "wiki.wiki.doctype.wiki_page.wiki_page.update_page_settings",
+          args: {
+            name,
+            hide_on_sidebar: !!hideOnSidebar,
+          },
+        })
+        .then(() => {
+          frappe.show_alert({
+            message: __("Page settings updated successfully"),
+            indicator: "green",
+          });
+
+          $("#pageSettingsModal").modal("hide");
         });
     });
   }
