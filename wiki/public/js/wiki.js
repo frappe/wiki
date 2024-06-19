aedd7911414bad6e22fe15fa9d2f02a0d87c6ab4
Jannat Patel <pateljannat2308@gmail.com>
2024-06-19 17:32:56 +0530

    fix: sidebar scroll

--- wiki/public/js/wiki.js
+++ wiki/public/js/wiki.js
@@ -15,7 +15,8 @@ window.Wiki = class Wiki {
     let active_sidebar_item = $(".doc-sidebar .sidebar-item.active");
     if (active_sidebar_item.length > 0) {
       setTimeout(function () {
-        active_sidebar_item.get(0).scrollIntoView(false);
+        let position = active_sidebar_item.offset().top - $(".doc-sidebar").offset().top;
+        $(".doc-sidebar .web-sidebar").get(0).scrollTo(0, position);
       }, 50);
     }
   }
