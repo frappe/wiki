1343a524337bbf0815bd0407ff282ac70e96c8eb
Aysha <ayshapp468@gmail.com>
2024-12-10 17:32:15 +0530

    fix: amend condition to list wiki in /apps

--- wiki/utils.py
+++ wiki/utils.py
@@ -4,7 +4,7 @@ import frappe
 def check_app_permission():
 	"""Check if user has permission to access the app (for showing the app on app screen)"""
 
-	if frappe.session.user != "Administrator":
+	if frappe.session.user == "Administrator":
 		return True
 
 	roles = frappe.get_roles()
