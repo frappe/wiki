0f6feb0a65f8db694f5bd670f28df6ee1bb01a0a
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-12-11 15:48:56 +0530

    feat: Broken Links Report

---
+++ wiki/wiki/report/wiki_broken_links/test_broken_link_checker.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2024, Frappe and Contributors
+# See license.txt
+
+import frappe
+from frappe.tests.utils import FrappeTestCase
+
+from wiki.wiki.report.wiki_broken_links.wiki_broken_links import execute, get_broken_links
+
+BROKEN_LINK = "https://frappewiki.notavalidtld"
+
+TEST_MD_WITH_BROKEN_LINK = f"""
+## Hello
+
+This is a test for a [broken link]({BROKEN_LINK}).
+
+This is a [valid link](https://frappe.io).
+"""
+
+
+class TestWikiBrokenLinkChecker(FrappeTestCase):
+	def setUp(self):
+		frappe.db.delete("Wiki Page")
+		self.test_wiki_page = frappe.get_doc(
+			{
+				"doctype": "Wiki Page",
+				"content": TEST_MD_WITH_BROKEN_LINK,
+				"title": "My Wiki Page",
+				"route": "test-wiki-page-route",
+			}
+		).insert()
+
+		self.test_wiki_space = frappe.get_doc({"doctype": "Wiki Space", "route": "test-ws-route"}).insert()
+
+	def test_returns_correct_broken_links(self):
+		broken_links = get_broken_links(TEST_MD_WITH_BROKEN_LINK)
+		self.assertEqual(len(broken_links), 1)
+
+	def test_wiki_broken_link_report(self):
+		_, data = execute()
+		self.assertEqual(len(data), 1)
+		self.assertEqual(data[0]["broken_link"], BROKEN_LINK)
+
+	def test_wiki_broken_list_report_with_filters(self):
+		_, data = execute({"wiki_space": self.test_wiki_space.name})
+		self.assertEqual(len(data), 0)
+
+		self.test_wiki_space.append(
+			"wiki_sidebars", {"wiki_page": self.test_wiki_page, "parent_label": "Test Parent Label"}
+		)
+		self.test_wiki_space.save()
+
+		_, data = execute({"wiki_space": self.test_wiki_space.name})
+		self.assertEqual(len(data), 1)
+		self.assertEqual(data[0]["wiki_page"], self.test_wiki_page.name)
+		self.assertEqual(data[0]["broken_link"], BROKEN_LINK)
+
+	def tearDown(self):
+		frappe.db.rollback()
---
+++ wiki/wiki/report/wiki_broken_links/wiki_broken_links.py
@@ -0,0 +1,87 @@
+# Copyright (c) 2024, Frappe and contributors
+# For license information, please see license.txt
+
+import frappe
+import requests
+from bs4 import BeautifulSoup
+from frappe import _
+
+
+def execute(filters: dict | None = None):
+	"""Return columns and data for the report.
+
+	This is the main entry point for the report. It accepts the filters as a
+	dictionary and should return columns and data. It is called by the framework
+	every time the report is refreshed or a filter is updated.
+	"""
+	columns = get_columns()
+	data = get_data(filters)
+
+	return columns, data
+
+
+def get_columns() -> list[dict]:
+	"""Return columns for the report.
+
+	One field definition per column, just like a DocType field definition.
+	"""
+	return [
+		{
+			"label": _("Wiki Page"),
+			"fieldname": "wiki_page",
+			"fieldtype": "Link",
+			"options": "Wiki Page",
+			"width": 200,
+		},
+		{
+			"label": _("Broken Link"),
+			"fieldname": "broken_link",
+			"fieldtype": "Data",
+			"options": "URL",
+			"width": 400,
+		},
+	]
+
+
+def get_data(filters: dict | None = None) -> list[list]:
+	"""Return data for the report.
+
+	The report data is a list of rows, with each row being a list of cell values.
+	"""
+	data = []
+
+	if not filters:
+		wiki_pages = frappe.db.get_all("Wiki Page", fields=["name", "content"])
+	elif filters.get("wiki_space"):
+		wiki_space = filters.get("wiki_space")
+		wiki_pages = frappe.db.get_all(
+			"Wiki Group Item",
+			fields=["wiki_page as name", "wiki_page.content as content"],
+			filters={"parent": wiki_space, "parenttype": "Wiki Space"},
+		)
+
+	for page in wiki_pages:
+		broken_links_for_page = get_broken_links(page.content)
+		rows = [{"broken_link": link, "wiki_page": page["name"]} for link in broken_links_for_page]
+		data.extend(rows)
+
+	return data
+
+
+def get_broken_links(md_content: str):
+	html = frappe.utils.md_to_html(md_content)
+	soup = BeautifulSoup(html, "html.parser")
+
+	links = soup.find_all("a")
+	images = soup.find_all("img")
+	broken_links = []
+	for el in links + images:
+		url = el.attrs.get("href") or el.attrs.get("src")
+		try:
+			response = requests.head(url, verify=False, timeout=5)
+			if response.status_code >= 400:
+				broken_links.append(url)
+		except Exception:
+			broken_links.append(url)
+
+	return broken_links
