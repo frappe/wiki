b45b06d3e82c55609cdbb3eed28116a24204cbb0
stravo1@mac <stravoone@gmail.com>
2025-03-28 20:46:02 +0530

    fix: update page title in the DOM when loading wiki content

--- wiki/public/js/wiki.js
+++ wiki/public/js/wiki.js
@@ -288,6 +288,8 @@ function loadWikiPage(url, pageElement, replaceState = false) {
         $(".wiki-content").html(r.message.content);
 
         $(".wiki-title").html(r.message.title);
+        
+        $('title').text(r.message.title);
 
         if (r.message.toc_html) {
           $(".page-toc .list-unstyled").html(r.message.toc_html);
