1e80dfaf29157082d33d7011b5e88acb81f6e3e3
Hussain Nagaria <hussainbhaitech@gmail.com>
2026-05-07 14:13:39 +0530

    fix: make v3 wiki migration resumable

--- wiki/wiki/doctype/wiki_page/wiki_page.py
+++ wiki/wiki/doctype/wiki_page/wiki_page.py
@@ -12,15 +12,29 @@ class WikiPage(WebsiteGenerator):
 			)
 		)
 
-	def _migrate_to_wiki_document(self):
-		frappe.get_doc(
-			{
-				"doctype": "Wiki Document",
-				"title": self.title,
-				"content": self.content,
-				"route": self.route,
-				"is_group": 0,
-				"is_published": self.published,
-				"is_private": not self.allow_guest,
-			}
-		).insert()
+	def _migrate_to_wiki_document(self, parent_wiki_document=None, sort_order=None):
+		existing_name = frappe.db.get_value("Wiki Document", {"route": self.route, "is_group": 0}, "name")
+		if existing_name:
+			wiki_document = frappe.get_doc("Wiki Document", existing_name)
+		else:
+			wiki_document = frappe.new_doc("Wiki Document")
+
+		wiki_document.title = self.title
+		wiki_document.content = self.content
+		wiki_document.route = self.route
+		wiki_document.is_group = 0
+		wiki_document.is_published = self.published
+		wiki_document.is_private = not self.allow_guest
+
+		if parent_wiki_document is not None:
+			wiki_document.parent_wiki_document = parent_wiki_document
+
+		if sort_order is not None:
+			wiki_document.sort_order = sort_order
+
+		if existing_name:
+			wiki_document.save(ignore_permissions=True)
+		else:
+			wiki_document.insert(ignore_permissions=True)
+
+		return wiki_document
